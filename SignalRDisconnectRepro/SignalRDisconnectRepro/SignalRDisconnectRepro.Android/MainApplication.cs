b178377e27698d9f1895c20a295565bf8d903302
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-05-03 11:48:42 +0200

    repro not reproing

---
+++ SignalRDisconnectRepro/SignalRDisconnectRepro/SignalRDisconnectRepro.Android/MainApplication.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Plugin.CurrentActivity;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SignalRDisconnectRepro.Droid
+{
+    [Application]
+    public class MainApplication : Application, Application.IActivityLifecycleCallbacks
+    {
+        private HubConnection Connection;
+
+        public MainApplication(IntPtr handle, JniHandleOwnership transer)
+            :base(handle, transer)
+        {
+            TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;  
+        }
+
+        private static void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+        }
+        
+        public override void OnCreate()
+        {
+            
+            Connection = new HubConnectionBuilder()
+                .WithUrl("https://s1.blauhaustechnology.com:5201/")
+                .Build();
+
+
+            base.OnCreate();
+            RegisterActivityLifecycleCallbacks(this);
+            CrossCurrentActivity.Current.Init(this);
+
+        }
+
+        public override void OnTerminate()
+        {
+            base.OnTerminate();
+            UnregisterActivityLifecycleCallbacks(this);
+        }
+
+        public async void OnActivityCreated(Activity activity, Bundle savedInstanceState)
+        {
+            CrossCurrentActivity.Current.Activity = activity;
+        }
+
+        public void OnActivityDestroyed(Activity activity)
+        {
+        }
+
+        public async void OnActivityPaused(Activity activity)
+        {
+            await Connection.StopAsync();
+        }
+
+        public async void OnActivityResumed(Activity activity)
+        {
+            await Connection.StartAsync();
+            CrossCurrentActivity.Current.Activity = activity;
+        }
+
+        public void OnActivitySaveInstanceState(Activity activity, Bundle outState)
+        {
+        }
+
+        public void OnActivityStarted(Activity activity)
+        {
+        }
+
+        public void OnActivityStopped(Activity activity)
+        {
+        }
+    }
+}
---
+++ SignalRDisconnectRepro/SignalRDisconnectRepro/SignalRDisconnectRepro/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.AspNetCore.SignalR.Client;
+using Xamarin.Forms;
+
+namespace SignalRDisconnectRepro
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
